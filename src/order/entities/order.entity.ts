a11cf0f7f452a345b90ee6c865e705fedfac91cb
CuongDo2010 <24100332@st.phenikaa-uni.edu.vn>
2026-06-20 13:18:28 +0700

    Update order.entity.ts

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,1 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
 
+@Entity('orders')
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: 'Khách lẻ' })
+  customer_name: string;
+
+  @Column('decimal')
+  total_amount: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
